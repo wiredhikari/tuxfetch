9d8136dfc99ae348a22789179648bf88e18152f4
Atharva Amritkar <atharvaamritkar@protonmail.com>
2022-05-17 01:38:14 +0530

    Updating hashed

--- default.nix
+++ default.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "wiredhikari";
     repo = pname;
     rev = version;
-    sha256 = "1hqps7l5qrjh9f914r5i6kmcz6f1yb951nv4lby0cjnp5l253kps";
+    sha256 = "1xlcnyy7zcnq78d9fv6k3vsawvbwzvailp21qskzsbpx299h3wpc";
   };
 
-  cargoSha256 = "03wf9r2csi6jpa7v5sw5lpxkrk4wfzwmzx7k3991q3bdjzcwnnwp";
+  cargoSha256 = "161nhazif24f7m555khmdvp9as2kndd47aisi8nbdwx91advyzkf";
 
   meta = with lib; {
     description = " A command-line system information tool written in rust";
